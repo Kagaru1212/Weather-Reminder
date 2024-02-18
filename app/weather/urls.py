8a950ebc54fc2653ed6bbd8e1da35f9dc2e1dcb2
Oleksandr Pinchuk <@foxstudent106121>
2024-02-18 18:45:28 +0300

    commit01

--- app/weather/urls.py
+++ app/weather/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/weather', views.WeatherApiView.as_view()),
-    path('api/v1/weather/<int:pk>/', views.WeatherApiUpdate.as_view()),
+    path('api/v1/subscribing/', views.WeatherApiView.as_view()),
+    path('api/v1/subscribing/<int:pk>/', views.WeatherApiUpdate.as_view()),
+    path('api/v1/subscribing/<int:pk>/weather/', views.weather),
 ]
--- app/weather/views.py
+++ app/weather/views.py
@@ -1,3 +1,6 @@
+import requests
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
 from rest_framework import generics
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
@@ -34,3 +37,27 @@ class WeatherApiUpdate(generics.RetrieveUpdateDestroyAPIView):
 
     def perform_update(self, serializer):
         serializer.save()
+
+
+def weather(request, pk):
+    appid = '6dabbdf16a4a011fc0e41ae5cb097de4'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    # Получаем информацию о текущем пользователе из запроса
+    user = request.user
+
+    # Проверяем, есть ли у пользователя подписка
+    try:
+        subscribing = Subscribing.objects.get(user=user, pk=pk)
+        city = subscribing.city_name  # Получаем имя города из подписки
+        res = requests.get(url.format(city)).json()
+
+        city_info = {
+            'city': city,
+            'temp': res['main']['temp'],
+            'humidity': res['main']['humidity'],
+        }
+
+        return JsonResponse(city_info)
+    except ObjectDoesNotExist:
+        return JsonResponse({'error': 'User has no subscription with specified pk'}, status=400)
